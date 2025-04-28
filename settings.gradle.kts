4fa7cc0c9759571a88d696bd3ef872d6eb88c1bb
dioq <ptttqq@gmail.com>
2025-04-28 16:38:54 +0800

    转移4个module

---
+++ settings.gradle.kts
@@ -0,0 +1,26 @@
+pluginManagement {
+    repositories {
+        google {
+            content {
+                includeGroupByRegex("com\\.android.*")
+                includeGroupByRegex("com\\.google.*")
+                includeGroupByRegex("androidx.*")
+            }
+        }
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "Android_UI"
+include(":webview01")
+include(":viewdemo3")
+include(":viewdemo2")
+include(":viewdemo1")
